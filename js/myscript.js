36844888fd947ae66bae4ae0f807dab9209ed0a6
Donatella7 <campaniellodonatella@gmail.com>
2022-03-16 17:50:47 +0100

    comment

--- js/myscript.js
+++ js/myscript.js
@@ -1,21 +1,25 @@
 const app = new Vue(
     {
         el:'#app',
-
+        
         data: {
             welcomeMsg: 'Hello Vue.js',
             imageUrl:'https://picsum.photos/200',
             linkImageInput: "",
             myImgDisplay:"d-none",
         },
-
+        
         methods: {
             getImage: function(){
                 this.myImgDisplay="d-block";
-                console.log("click");
-                console.log(this.linkImageInput);
                 return this.linkImageInput;
-            }
+            },
+            
         }
-    }       
-);
+        
+        
+        
+    } 
+    
+    );
+    
