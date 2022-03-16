608303ed38b9f9bd665ebc20da643918af7e8dfb
Donatella7 <campaniellodonatella@gmail.com>
2022-03-16 15:27:46 +0100

    image

--- js/myscript.js
+++ js/myscript.js
@@ -3,7 +3,8 @@ const app = new Vue(
         el:'#app',
 
         data: {
-            welcomeMsg: 'Hello Vue.js'
+            welcomeMsg: 'Hello Vue.js',
+            imageUrl:'https://picsum.photos/200',
         },
 
     }
